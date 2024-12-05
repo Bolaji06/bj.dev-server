c275e1b4de60ac4865b089fe4e8fec9020acca1f
Bolaji06 <joshspencer0603@gmail.com>
2024-12-05 22:40:41 +0100

    resolve deployment error

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ import email from "./src/routes/email.js";
 import auth from "./src/routes/auth.js";
 import user from "./src/routes/user.js";
 import experience from "./src/routes/experience.js";
-import chat from "./src/routes/chat.js"
 import profile from "./src/routes/profile.js";
 
 const app = express();
@@ -55,8 +54,7 @@ app.use("/api/send-email", email);
 app.use("/api/auth", auth);
 app.use("/api/user", user);
 app.use("/api/experience", experience);
-app.use("/api/chat", chat);
-app.use("/api/profile", profile)
+app.use("/api/profile", profile);
 
 app.listen(7000, () => {
   console.log("server starting at 7000");
